3e056dd40dbb006dfa206bf87dee4bb41df0f83f
LorisHuetter <tidepodislovetidepodislife@gmail.com>
2021-08-26 15:58:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function blubber(){
+  return "epic test blubber"
+}
 
 function App() {
+  function blubberoutput(){
+    console.log(blubber())
+  }
+  let [counter, setCounter] = useState(0)
+  function increaseCounter(){
+    setCounter(counter+1)
+  }
+  function decreaseCounter(){
+    setCounter(counter-1)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello, World!</p>
+        <button onClick={ blubberoutput }>Blubber</button>
+        <p><button onClick={ decreaseCounter }>Decrease</button> <button onClick={ increaseCounter }>Increase</button></p>
+        <p>{counter}</p>
+        <MyComponent />
       </header>
     </div>
   );
+}
+
+export function MyComponent(){
+  return(
+    <div>MyComponent</div>
+  )
 }
 
 export default App;
